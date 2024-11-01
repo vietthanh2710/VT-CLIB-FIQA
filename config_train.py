c794b05201c7835084014040b7ea0ec3fa0adfaf
vietthanh2710 <138222980+vietthanh2710@users.noreply.github.com>
2024-11-01 17:34:57 +0700

    Update config_train.py

--- config_train.py
+++ config_train.py
@@ -3,18 +3,18 @@ import torchvision.transforms as T
 
 class Config(object):
 # training dataset
-    data_list = f"./"
-    clip_model = './weights/RN50.pt'
+    data_list = f"/kaggle/input/data_list/pytorch/default/1/data_list.txt"
+    clip_model = '/kaggle/input/clib-fiqa/pytorch/default/1/RN50.pt'
 # save settings
-    checkpoints = f"./checkpoints"   
+    checkpoints = f"/kaggle/working/checkpoints"   
 # training settings
     device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
     seed = 323
     pin_memory = True
     num_workers = 12
-    batch_size = 256         
+    batch_size = 32         
     epoch = 25
-    lr = 1e-5
+    lr = 5e-6
     weight_decay = 0.001
     Epo_th = 5
     saveModel_epoch = 5
